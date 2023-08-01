09ddad0cb07e36f9a5ad08fe47aea3e1c7c85b81
Joseph Pareti <35771386+joepareti54@users.noreply.github.com>
2023-08-01 08:23:49 +0200

    Add files via upload

---
+++ VAE_JP_solution_clean.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+
+import keras
+from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Reshape
+from keras.models import Model
+from keras.datasets import mnist
+from keras import backend as K
+import numpy as np
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution(
+
+# Load MNIST dataset and preprocess
+(x_train, y_train), (x_test, y_test) = mnist.load_data(
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+img_width, img_height = x_train.shape[1], x_train.shape[2
+num_channels = 1  # MNIST --> grayscale, so 1 channel
+x_train = x_train.reshape(x_train.shape[0], img_height, img_width, num_channels
+x_test = x_test.reshape(x_test.shape[0], img_height, img_width, num_channels
+input_shape = (img_height, img_width, num_channels
+
+# Define VAE architecture
+latent_dim = 2
+
+input_img = Input(shape=input_shape, name='encoder_input'
+x = Conv2D(32, 3, padding='same', activation='relu')(input_img
+x = Conv2D(64, 3, padding='same', activation='relu', strides=(2, 2))(x
+x = Conv2D(64, 3, padding='same', activation='relu')(x
+x = Conv2D(64, 3, padding='same', activation='relu')(x
+conv_shape = K.int_shape(x)  # Shape of conv to be provided to decoder
+x = Flatten()(x
+x = Dense(32, activation='relu')(x
+z_mu = Dense(latent_dim, name='z_mu')(x
+z_sigma = Dense(latent_dim, name='z_sigma')(x
+
+# Custom function for sampling in the latent space
+def sampling(z_args)
+    z_mu, z_sigma = z_args
+    batch = K.shape(z_mu)[0
+    dim = K.int_shape(z_mu)[1
+    epsilon = K.random_normal(shape=(batch, dim)
+    return z_mu + K.exp(z_sigma / 2) * epsilon
+
+# Use the sampling function to create the z tensor
+z = sampling([z_mu, z_sigma]
+encoder = Model(input_img, [z_mu, z_sigma, z], name='encoder'
+print(encoder.summary()
+
+decoder_input = Input(shape=(latent_dim,), name='decoder_input'
+x = Dense(conv_shape[1] * conv_shape[2] * conv_shape[3], activation='relu')(decoder_input
+x = Reshape((conv_shape[1], conv_shape[2], conv_shape[3]))(x
+x = Conv2DTranspose(32, 3, padding='same', activation='relu', strides=(2, 2))(x
+x = Conv2DTranspose(num_channels, 3, padding='same', activation='sigmoid', name='decoder_output')(x
+
+decoder = Model(decoder_input, x, name='decoder'
+decoder.summary(
+
+z_decoded = decoder(z
+
+def vae_loss(_, z_decoded)
+    x = K.flatten(input_img
+    z_decoded = K.flatten(z_decoded
+    recon_loss = keras.metrics.binary_crossentropy(x, z_decoded
+    kl_loss = -5e-4 * K.mean(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1
+    return K.mean(recon_loss + kl_loss
+
+vae = Model(input_img, z_decoded, name='vae'
+vae.compile(optimizer='adam', loss=vae_loss
+vae.summary(
+
+vae.fit(x_train, x_train, epochs=10, batch_size=32, validation_split=0.2
+
+# =================
+# Visualize results
+# =================
+#Visualize inputs mapped to the Latent space
+#Remember that we have encoded inputs to latent space dimension = 2. 
+#Extract z_mu --> first parameter in the result of encoder prediction representing mean
+
+mu, _, _ = encoder.predict(x_test)
+#Plot dim1 and dim2 for mu
+plt.figure(figsize=(10, 10))
+plt.scatter(mu[:, 0], mu[:, 1], c=y_test, cmap='brg')
+plt.xlabel('dim 1')
+plt.ylabel('dim 2')
+plt.colorbar()
+plt.show()
+
+
+# Visualize images
+#Single decoded image with random input latent vector (of size 1x2)
+#Latent space range is about -5 to 5 so pick random values within this range
+#Try starting with -1, 1 and slowly go up to -1.5,1.5 and see how it morphs from 
+#one image to the other.
+sample_vector = np.array([[1,-1]])
+decoded_example = decoder.predict(sample_vector)
+decoded_example_reshaped = decoded_example.reshape(img_width, img_height)
+plt.imshow(decoded_example_reshaped)
+
+#Let us automate this process by generating multiple images and plotting
+#Use decoder to generate images by tweaking latent variables from the latent space
+#Create a grid of defined size with zeros. 
+#Take sample from some defined linear space. In this example range [-4, 4]
+#Feed it to the decoder and update zeros in the figure with output.
+
+
+n = 20  # generate 15x15 digits
+figure = np.zeros((img_width * n, img_height * n, num_channels))
+
+#Create a Grid of latent variables, to be provided as inputs to decoder.predict
+#Creating vectors within range -5 to 5 as that seems to be the range in latent space
+grid_x = np.linspace(-5, 5, n)
+grid_y = np.linspace(-5, 5, n)[::-1]
+
+# decoder for each square in the grid
+for i, yi in enumerate(grid_y):
+    for j, xi in enumerate(grid_x):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = decoder.predict(z_sample)
+        digit = x_decoded[0].reshape(img_width, img_height, num_channels)
+        figure[i * img_width: (i + 1) * img_width,
+               j * img_height: (j + 1) * img_height] = digit
+
+plt.figure(figsize=(10, 10))
+#Reshape for visualization
+fig_shape = np.shape(figure)
+figure = figure.reshape((fig_shape[0], fig_shape[1]))
+
+plt.imshow(figure, cmap='gnuplot2')
+plt.show()  
